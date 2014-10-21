1cfcfd0266caaf67449a49ef547605e6ce78711a
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2014-10-21 19:08:02 +0200

    Handle bus error

--- src/player.py
+++ src/player.py
@@ -59,8 +59,7 @@ class Player(GObject.GObject):
 		
 		self._bus = self._player.get_bus()
 		self._bus.add_signal_watch()
-		#self._bus.connect('message::state-changed', self._on_bus_state_changed)
-		#self.bus.connect('message::error', self._onBusError)
+		self.bus.connect('message::error', self._on_bus_eos)
 		self._bus.connect('message::eos', self._on_bus_eos)
 		
 	"""
