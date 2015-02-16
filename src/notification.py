9a5304176bfb5728a4bc3f4921a4259f197a7824
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2015-02-16 16:36:37 +0100

    notification.py: pep8

--- src/notification.py
+++ src/notification.py
@@ -11,12 +11,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from gi.repository import GLib, Gtk, Notify
+from gi.repository import GLib, Notify
 from gettext import gettext as _
 
-from lollypop.define import *
-from lollypop.albumart import AlbumArt
-from lollypop.utils import translate_artist_name
+from lollypop.define import Objects, ART_SIZE_BIG
+
 
 class NotificationManager:
 
@@ -26,20 +25,28 @@ class NotificationManager:
     def __init__(self):
 
         caps = Notify.get_server_caps()
-        
+
         self._notification = Notify.Notification()
         self._notification.set_category('x-gnome.music')
         if "action-icons" in caps:
-            self._notification.set_hint('action-icons', GLib.Variant('b', True))
+            self._notification.set_hint('action-icons',
+                                        GLib.Variant('b', True))
         if "persistence" in caps:
-            self._notification.set_hint('resident', GLib.Variant('b', True))
-        self._notification.set_hint('desktop-entry', GLib.Variant('s', 'lollypop'))
+            self._notification.set_hint('resident',
+                                        GLib.Variant('b', True))
+        self._notification.set_hint('desktop-entry',
+                                    GLib.Variant('s', 'lollypop'))
         if "actions" in caps:
-            self._notification.add_action('media-skip-backward', _("Previous"),
-                                         self._go_previous, None)
-            self._notification.add_action('media-skip-forward', _("Next"),
-                                         self._go_next, None)
-        Objects.player.connect('current-changed', self._on_current_changed)
+            self._notification.add_action('media-skip-backward',
+                                          _("Previous"),
+                                          self._go_previous,
+                                          None)
+            self._notification.add_action('media-skip-forward',
+                                          _("Next"),
+                                          self._go_next,
+                                          None)
+        Objects.player.connect('current-changed',
+                               self._on_current_changed)
 
 #######################
 # PRIVATE             #
@@ -50,18 +57,23 @@ class NotificationManager:
         @param player Player
     """
     def _on_current_changed(self, player):
-        self._notification.set_hint('image-path', GLib.Variant('s', Objects.art.get_path(player.current.album_id, ART_SIZE_BIG)))
+        self._notification.set_hint('image-path',
+                                    GLib.Variant('s',
+                                                 Objects.art.get_path(
+                                                    player.current.album_id,
+                                                    ART_SIZE_BIG)))
         self._notification.update(player.current.title,
-                                  # TRANSLATORS: by refers to the artist, from to the album
-                                  _("by %s, from %s") % ('<b>' + player.current.artist + '</b>',
-                                                         '<i>' + player.current.album + '</i>'),
+                                  # TRANSLATORS: by refers to the artist,
+                                  # from to the album
+                                  _("by %s, from %s") %
+                                  ('<b>' + player.current.artist + '</b>',
+                                   '<i>' + player.current.album + '</i>'),
                                   'lollypop')
         try:
             self._notification.show()
         except:
             pass
 
-   
     """
         Callback for notification prev button
     """
