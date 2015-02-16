af5fe7cffde4794abb19b55c29ee21218e23941a
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2015-02-16 15:47:00 +0100

    database_genres.py: pep8

--- src/database_genres.py
+++ src/database_genres.py
@@ -15,33 +15,35 @@ from gettext import gettext as _
 
 from lollypop.define import Objects
 
-"""
-    All functions take a sqlite cursor as last parameter, set another one if you're in a thread
-"""
+
+# All functions take a sqlite cursor as last parameter,
+# set another one if you're in a thread
 class DatabaseGenres:
     def __init__(self):
         pass
-    
+
     """
         Add a new genre to database
         @param Name as string
         @warning: commit needed
     """
-    def add(self, name, sql = None):
+    def add(self, name, sql=None):
         if not sql:
             sql = Objects.sql
-        sql.execute("INSERT INTO genres (name) VALUES (?)", (name,))
-        
+        sql.execute("INSERT INTO genres (name) VALUES (?)",
+                    (name,))
 
     """
         Get genre id for name
         @param name as string
         @return genre id as int
     """
-    def get_id(self, name, sql = None):
+    def get_id(self, name, sql=None):
         if not sql:
             sql = Objects.sql
-        result = sql.execute("SELECT rowid from genres where name=? COLLATE NOCASE", (name,))
+        result = sql.execute("SELECT rowid FROM genres\
+                              WHERE name=?\
+                              COLLATE NOCASE", (name,))
         v = result.fetchone()
         if v and len(v) > 0:
             return v[0]
@@ -53,26 +55,27 @@ class DatabaseGenres:
         @param string
         @return int
     """
-    def get_name(self, genre_id, sql = None):
+    def get_name(self, genre_id, sql=None):
         if not sql:
             sql = Objects.sql
-        result = sql.execute("SELECT name from genres where rowid=?", (genre_id,))
+        result = sql.execute("SELECT name FROM genres\
+                              WHERE rowid=?", (genre_id,))
         v = result.fetchone()
         if v and len(v) > 0:
             return v[0]
 
         return _("Unknown")
 
-
     """
         Get all availables albums  for genres
         @return Array of id as int
     """
-    def get_albums(self, genre_id, sql = None):
+    def get_albums(self, genre_id, sql=None):
         if not sql:
             sql = Objects.sql
         albums = []
-        result = sql.execute("SELECT rowid FROM albums where genre_id=?",(genre_id,))
+        result = sql.execute("SELECT rowid FROM albums\
+                             WHERE genre_id=?", (genre_id,))
         for row in result:
             albums += row
         return albums
@@ -81,13 +84,12 @@ class DatabaseGenres:
         Get all availables genres
         @return Array of (id as int, name as string)]
     """
-    def get(self, sql = None):
+    def get(self, sql=None):
         if not sql:
             sql = Objects.sql
         genres = []
-        result = sql.execute("SELECT rowid, name FROM genres ORDER BY name COLLATE NOCASE")
+        result = sql.execute("SELECT rowid, name FROM genres\
+                              ORDER BY name COLLATE NOCASE")
         for row in result:
             genres += (row,)
         return genres
-
-
