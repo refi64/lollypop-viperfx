ddfd4dc373f12768db193a857de927ca7a9c7286
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2015-02-17 07:47:03 +0100

    window.py: pep8

--- src/window.py
+++ src/window.py
@@ -11,43 +11,46 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from gi.repository import Gtk, Gdk, Gio, GLib
-from gettext import gettext as _, ngettext
+from gi.repository import Gtk, Gio, GLib
+from gettext import gettext as _
 from _thread import start_new_thread
 from os import environ
 
-from lollypop.define import Objects
+from lollypop.define import Objects, POPULARS, ALL, PLAYLISTS
+from lollypop.define import COMPILATIONS
 from lollypop.collectionscanner import CollectionScanner
 from lollypop.toolbar import Toolbar
-from lollypop.database import Database
 from lollypop.selectionlist import SelectionList
 from lollypop.playlists import PlaylistsManager
-from lollypop.player import Player
-from lollypop.view import *
+from lollypop.view import AlbumView, ArtistView
+from lollypop.view import PlaylistView, PlaylistConfigureView, LoadingView
 
+
+# Main window
 class Window(Gtk.ApplicationWindow):
-
     """
         Init window objects
-    """    
+    """
     def __init__(self, app):
         Gtk.ApplicationWindow.__init__(self,
-                           application=app,
-                           title=_("Lollypop"))
+                                       application=app,
+                                       title="Lollypop")
 
         self._timeout = None
-        # SelectionList::update() is based on index in values tuple. For db objects, it's rowid so
-        # it will works. For playlists, we just use a static int and increment it over time
+        # SelectionList::update() is based on index in values tuple.
+        # For db objects, it's rowid so it will works.
+        # For playlists, we just use a static int and increment it over time
         self._counter = 0
-        # Playlist do not have id (not in db), so we use an index to retrieve playlist name
+        # Playlist do not have id (not in db),
+        # so we use an index to retrieve playlist name
         self._playlists = []
 
-        self._setup_window()                
+        self._setup_window()
         self._setup_view()
 
         if not self._setup_scanner():
             self._setup_lists(False)
-            self._list_one.widget.show()            
+            self._list_one.widget.show()
             if Objects.settings.get_value('save-state'):
                 self._restore_view_state()
             else:
@@ -59,11 +62,13 @@ class Window(Gtk.ApplicationWindow):
         ids = []
         for setting in party_settings:
             if isinstance(setting, int):
-                ids.append(setting)    
+                ids.append(setting)
         Objects.player.set_party_ids(ids)
         self.connect("destroy", self._on_destroyed_window)
-        Objects.playlists.connect("playlists-changed", self._update_lists)
-        Objects.playlists.connect("playlist-changed", self._update_view_playlist)
+        Objects.playlists.connect("playlists-changed",
+                                  self._update_lists)
+        Objects.playlists.connect("playlist-changed",
+                                  self._update_view_playlist)
 
     """
         Update music database
@@ -78,6 +83,7 @@ class Window(Gtk.ApplicationWindow):
         self._stack.set_visible_child(view)
         self.save_view_state()
         self._scanner.update(self._progress, False)
+        self._clean_view(old_view)
 
     """
         Update view class
@@ -101,8 +107,14 @@ class Window(Gtk.ApplicationWindow):
         Save view state
     """
     def save_view_state(self):
-        Objects.settings.set_value("list-one", GLib.Variant('i', self._list_one.get_selected_item()))
-        Objects.settings.set_value("list-two", GLib.Variant('i', self._list_two.get_selected_item()))
+        Objects.settings.set_value("list-one",
+                                   GLib.Variant(
+                                        'i',
+                                        self._list_one.get_selected_item()))
+        Objects.settings.set_value("list-two",
+                                   GLib.Variant(
+                                        'i',
+                                        self._list_two.get_selected_item()))
 
 ############
 # Private  #
@@ -121,7 +133,7 @@ class Window(Gtk.ApplicationWindow):
     """
         Run collection update if needed
         @return True if hard scan is running
-    """    
+    """
     def _setup_scanner(self):
         self._scanner = CollectionScanner()
         self._scanner.connect("scan-finished", self._update_lists)
@@ -157,7 +169,7 @@ class Window(Gtk.ApplicationWindow):
         # Do not update if updater is PlaylistsManager
         if not isinstance(updater, PlaylistsManager):
             self._setup_lists(True)
-        
+
     """
         Update list two
         @param updater as GObject
@@ -173,13 +185,14 @@ class Window(Gtk.ApplicationWindow):
         Setup media player keys
     """
     def _setup_media_keys(self):
-        self._proxy = Gio.DBusProxy.new_sync(Gio.bus_get_sync(Gio.BusType.SESSION, None),
-                                             Gio.DBusProxyFlags.NONE,
-                                             None,
-                                             'org.gnome.SettingsDaemon',
-                                             '/org/gnome/SettingsDaemon/MediaKeys',
-                                             'org.gnome.SettingsDaemon.MediaKeys',
-                                             None)
+        self._proxy = Gio.DBusProxy.new_sync(
+                            Gio.bus_get_sync(Gio.BusType.SESSION, None),
+                            Gio.DBusProxyFlags.NONE,
+                            None,
+                            'org.gnome.SettingsDaemon',
+                            '/org/gnome/SettingsDaemon/MediaKeys',
+                            'org.gnome.SettingsDaemon.MediaKeys',
+                            None)
         self._grab_media_player_keys()
         try:
             self._proxy.connect('g-signal', self._handle_media_keys)
@@ -193,10 +206,10 @@ class Window(Gtk.ApplicationWindow):
     def _grab_media_player_keys(self):
         try:
             self._proxy.call_sync('GrabMediaPlayerKeys',
-                                 GLib.Variant('(su)', ('Lollypop', 0)),
-                                 Gio.DBusCallFlags.NONE,
-                                 -1,
-                                 None)
+                                  GLib.Variant('(su)', ('Lollypop', 0)),
+                                  Gio.DBusCallFlags.NONE,
+                                  -1,
+                                  None)
         except GLib.GError:
             # We cannot grab media keys if no settings daemon is running
             pass
@@ -206,7 +219,8 @@ class Window(Gtk.ApplicationWindow):
     """
     def _handle_media_keys(self, proxy, sender, signal, parameters):
         if signal != 'MediaPlayerKeyPressed':
-            print('Received an unexpected signal \'%s\' from media player'.format(signal))
+            print('Received an unexpected signal\
+                   \'%s\' from media player'.format(signal))
             return
         response = parameters.get_child_value(1).get_string()
         if 'Play' in response:
@@ -217,21 +231,22 @@ class Window(Gtk.ApplicationWindow):
             Objects.player.next()
         elif 'Previous' in response:
             Objects.player.prev()
-    
+
     """
         Setup window icon, position and size, callback for updating this values
     """
     def _setup_window(self):
         self.set_icon_name('lollypop')
         size_setting = Objects.settings.get_value('window-size')
-        if isinstance(size_setting[0], int) and isinstance(size_setting[1], int):
+        if isinstance(size_setting[0], int) and\
+           isinstance(size_setting[1], int):
             self.resize(size_setting[0], size_setting[1])
         else:
             self.set_size_request(800, 600)
         position_setting = Objects.settings.get_value('window-position')
-        if len(position_setting) == 2 \
-            and isinstance(position_setting[0], int) \
-            and isinstance(position_setting[1], int):
+        if len(position_setting) == 2 and\
+           isinstance(position_setting[0], int) and\
+           isinstance(position_setting[1], int):
             self.move(position_setting[0], position_setting[1])
 
         if Objects.settings.get_value('window-maximized'):
@@ -251,10 +266,12 @@ class Window(Gtk.ApplicationWindow):
         self._paned_list_view = Gtk.HPaned()
         vgrid = Gtk.Grid()
         vgrid.set_orientation(Gtk.Orientation.VERTICAL)
-    
+
         self._toolbar = Toolbar()
         self._toolbar.header_bar.show()
-        self._toolbar.get_view_genres_btn().connect("toggled", self._on_genres_btn_toggled)
+        self._toolbar.get_view_genres_btn().connect(
+                                            "toggled",
+                                            self._on_genres_btn_toggled)
         self._show_genres = self._toolbar.get_view_genres_btn().get_active()
 
         self._list_one = SelectionList()
@@ -299,8 +316,12 @@ class Window(Gtk.ApplicationWindow):
         self._paned_list_view.add2(vgrid)
         self._paned_main_list.add1(self._list_one.widget)
         self._paned_main_list.add2(self._paned_list_view)
-        self._paned_main_list.set_position(Objects.settings.get_value("paned-mainlist-width").get_int32())
-        self._paned_list_view.set_position(Objects.settings.get_value("paned-listview-width").get_int32())
+        self._paned_main_list.set_position(
+                        Objects.settings.get_value(
+                                "paned-mainlist-width").get_int32())
+        self._paned_list_view.set_position(
+                        Objects.settings.get_value(
+                                "paned-listview-width").get_int32())
         self._paned_main_list.show()
         self._paned_list_view.show()
 
@@ -313,7 +334,7 @@ class Window(Gtk.ApplicationWindow):
         items.append((PLAYLISTS, _("Playlists")))
         items.append((ALL, _("All artists")))
         return items
-        
+
     """
         Setup genres/artists lists
         @param update as bool, if True, just update entries
@@ -343,20 +364,21 @@ class Window(Gtk.ApplicationWindow):
         @param update as bool, if True, just update entries
     """
     def _setup_list_artists(self, selection_list, genre_id, update):
-        if selection_list == self._list_one and self._list_two.widget.is_visible():
+        if selection_list == self._list_one and\
+           self._list_two.widget.is_visible():
             self._list_two.widget.hide()
             self._list_two.visible = False
             self._list_two.clear()
-            
+
         items = []
         selection_list.mark_as_artists(True)
         if selection_list == self._list_one:
             items = self._get_headers()
         if len(Objects.albums.get_compilations(genre_id)) > 0:
             items.append((COMPILATIONS, _("Compilations")))
-            
+
         items += Objects.artists.get(genre_id)
-        
+
         if update:
             selection_list.update(items)
         else:
@@ -375,7 +397,7 @@ class Window(Gtk.ApplicationWindow):
             self._playlists.append((self._counter, playlist_name))
             self._counter += 1
         values = self._playlists
-    
+
         # Do not show list if empty
         if len(values) > 0:
             if update:
@@ -425,7 +447,7 @@ class Window(Gtk.ApplicationWindow):
         playlist_name = None
         old_view = self._stack.get_visible_child()
 
-        if playlist_id != None:
+        if playlist_id is not None:
             for item in self._playlists:
                 if item[0] == playlist_id:
                     playlist_name = item[1]
@@ -456,7 +478,7 @@ class Window(Gtk.ApplicationWindow):
                 self._stack.set_visible_child(view)
                 start_new_thread(view.populate, ())
                 self._clean_view(old_view)
-    
+
     """
         Clear selection list
         @param selection list as SelectionList
@@ -466,7 +488,7 @@ class Window(Gtk.ApplicationWindow):
         selection_list.disconnect_by_func(callback)
         selection_list.clear()
         selection_list.connect('item-selected', callback)
-        
+
     """
         Clean view
         @param view as View
@@ -477,7 +499,7 @@ class Window(Gtk.ApplicationWindow):
             self._stack.remove(view)
             view.remove_signals()
             view.destroy()
-    
+
     """
         Update view based on selected object
         @param list as SelectionList
@@ -514,7 +536,8 @@ class Window(Gtk.ApplicationWindow):
         if self._list_one.get_selected_id() == PLAYLISTS:
             self._update_view_playlists(object_id)
         else:
-            self._update_view_detailed(object_id, self._list_one.get_selected_id())
+            self._update_view_detailed(object_id,
+                                       self._list_one.get_selected_id())
 
     """
         On genres button toggled, update lists/views
@@ -523,7 +546,7 @@ class Window(Gtk.ApplicationWindow):
         self._show_genres = self._toolbar.get_view_genres_btn().get_active()
         self._clear_list(self._list_one, self._on_list_one_selected)
         self._setup_lists(False)
-        
+
     """
         Update playlist view if we are in playlist view
         @param manager as PlaylistPopup
@@ -545,7 +568,7 @@ class Window(Gtk.ApplicationWindow):
         Delay event
         @param: widget as Gtk.Window
         @param: event as Gtk.Event
-    """        
+    """
     def _on_configure_event(self, widget, event):
         if self._timeout:
             GLib.source_remove(self._timeout)
@@ -558,21 +581,33 @@ class Window(Gtk.ApplicationWindow):
     def _save_size_position(self, widget):
         self._timeout = None
         size = widget.get_size()
-        Objects.settings.set_value('window-size', GLib.Variant('ai', [size[0], size[1]]))
+        Objects.settings.set_value('window-size',
+                                   GLib.Variant('ai',
+                                                [size[0], size[1]]))
+
         position = widget.get_position()
-        Objects.settings.set_value('window-position', GLib.Variant('ai', [position[0], position[1]]))
+        Objects.settings.set_value('window-position',
+                                   GLib.Variant('ai',
+                                                [position[0], position[1]]))
 
     """
         Save maximised state
     """
     def _on_window_state_event(self, widget, event):
-        Objects.settings.set_boolean('window-maximized', 'GDK_WINDOW_STATE_MAXIMIZED' in event.new_window_state.value_names)
+        Objects.settings.set_boolean('window-maximized',
+                                     'GDK_WINDOW_STATE_MAXIMIZED' in
+                                     event.new_window_state.value_names)
 
     """
         Save paned widget width
         @param widget as unused, data as unused
-    """    
+    """
     def _on_destroyed_window(self, widget):
-        Objects.settings.set_value("paned-mainlist-width", GLib.Variant('i', self._paned_main_list.get_position()))
-        Objects.settings.set_value("paned-listview-width", GLib.Variant('i', self._paned_list_view.get_position()))
-    
+        Objects.settings.set_value("paned-mainlist-width",
+                                   GLib.Variant(
+                                        'i',
+                                        self._paned_main_list.get_position()))
+        Objects.settings.set_value("paned-listview-width",
+                                   GLib.Variant(
+                                        'i',
+                                        self._paned_list_view.get_position()))
