8ccce715d71829b8e69eaae332b409804a7faee6
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2015-04-16 08:50:45 +0200

    Make sure progressbar expand

--- src/container.py
+++ src/container.py
@@ -212,6 +212,7 @@ class Container:
         self._list_two.connect('populated', self._on_list_two_populated)
 
         self._progress = Gtk.ProgressBar()
+        self._progress.set_property('expand', True)
 
         vgrid.add(self._stack)
         vgrid.add(self._progress)
