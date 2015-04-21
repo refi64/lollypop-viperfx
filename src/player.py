a2128f545a7422a010593d014862219fa18d3566
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2015-04-21 22:52:29 +0200

    Add missing import

--- src/player.py
+++ src/player.py
@@ -11,7 +11,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from gi.repository import GObject
+from gi.repository import GObject, GLib
 
 from lollypop.tagreader import TagReader
 from lollypop.player_base import BasePlayer
