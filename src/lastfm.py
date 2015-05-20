5befb35acb4ae2277dce1c6a035f2cce3f30bb3f
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2015-05-20 21:50:23 +0200

    Add a function for album cover download

--- src/lastfm.py
+++ src/lastfm.py
@@ -56,3 +56,31 @@ class LastFM:
             Lp.art.add_artist_img_to_cache(artist, url)
         except Exception as e:
             print("LastFM::download_artist_img: %s" % e)
+
+    """
+        Download album image
+        @param artist as string
+        @param album as string
+    """
+    def download_album_img(self, artist, album):
+        try:
+            response = urllib.request.urlopen(
+                                        "http://ws.audioscrobbler.com/2.0/?"
+                                        "method=artist.getinfo&api_key="
+                                        "7a9619a850ccf7377c46cf233c51e3c6"
+                                        "&artist=%s&album=%s&format=json" %\
+                                         urllib.parse.quote(artist,
+                                                            album))
+        except Exception as e:
+            print("LastFM::download_artist_img: %s" % e)
+            return
+        print('ici')
+        data = response.read()
+        decode = json.loads(data.decode("utf-8"))
+        if decode is None:
+            return
+        try:
+            url = decode['artist']['image'][3]['#text']
+            Lp.art.add_artist_img_to_cache(artist, url)
+        except Exception as e:
+            print("LastFM::download_artist_img: %s" % e)
