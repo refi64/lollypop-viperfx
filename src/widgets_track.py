364a44652c90c06c56cd1e0029a71dcf0d2327cd
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2015-06-05 07:32:21 +0200

    Change cover size to restore < 0.9.20 behaviour

--- src/widgets_track.py
+++ src/widgets_track.py
@@ -307,7 +307,7 @@ class TracksWidget(Gtk.ListBox):
         album_row.set_object_id(track_id)
         album_row.set_album_and_artist(album_id)
         album_id = Lp.tracks.get_album_id(track_id)
-        pixbuf = Lp.art.get_album(album_id, ArtSize.SMALL)
+        pixbuf = Lp.art.get_album(album_id, ArtSize.MEDIUM)
         album_row.set_cover(pixbuf, Lp.albums.get_name(album_id))
         album_row.show_cover(True)
         album_row.show()
