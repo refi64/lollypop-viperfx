50e844194e398d61aba3db9a847a2b1d8e022699
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2015-06-10 17:45:02 +0200

    Uneeded test

--- src/queue.py
+++ src/queue.py
@@ -33,6 +33,7 @@ class QueueWidget(Gtk.Popover):
 
         builder = Gtk.Builder()
         builder.add_from_resource('/org/gnome/Lollypop/QueueWidget.ui')
+        builder.connect_signals(self)
 
         self._model = Gtk.ListStore(GdkPixbuf.Pixbuf,  # Cover
                                     str,               # Artist
@@ -43,12 +44,11 @@ class QueueWidget(Gtk.Popover):
         self._view.set_model(self._model)
         self._view.set_property('fixed_height_mode', True)
 
-        builder.connect_signals(self)
-
         self._widget = builder.get_object('widget')
 
         renderer0 = Gtk.CellRendererPixbuf()
         renderer0.set_property('stock-size', ArtSize.MEDIUM)
+        stock_size = ArtSize.MEDIUM*2+ArtSize.SMALL_BORDER*2
         column0 = Gtk.TreeViewColumn("pixbuf1", renderer0, pixbuf=0)
         column0.set_sizing(Gtk.TreeViewColumnSizing.FIXED)
 
@@ -88,8 +88,13 @@ class QueueWidget(Gtk.Popover):
             artist_name = Lp.artists.get_name(artist_id)
             track_name = Lp.tracks.get_name(track_id)
             size = ArtSize.MEDIUM * self.get_scale_factor()
+            border = ArtSize.SMALL_BORDER * self.get_scale_factor()
             surface = Lp.art.get_album(album_id, size)
-            pixbuf = Gdk.pixbuf_get_from_surface(surface, 0, 0, size, size)
+            pixbuf = Gdk.pixbuf_get_from_surface(surface,
+                                                 0,
+                                                 0,
+                                                 size+border,
+                                                 size+border)
             del surface
             title = "<b>%s</b>\n%s" %\
                 (escape(artist_name),
