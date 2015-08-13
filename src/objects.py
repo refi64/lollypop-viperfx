dabfb9a1d707e7939bdf3b7a78bdfb9bce24ed9f
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2015-08-13 18:53:10 +0200

    Add a comment for __dir__()

--- src/objects.py
+++ src/objects.py
@@ -42,6 +42,9 @@ class Base:
         self.db = db
 
     def __dir__(self, *args, **kwargs):
+    """
+        Concatenate base class's fields with child class's fields
+    """
         return super(Base, self).__dir__(*args, **kwargs) + self.FIELDS
 
     def __getattr__(self, attr):
