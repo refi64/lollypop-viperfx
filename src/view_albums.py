c7eb370bd12a881f6e610edb706ec4c01025ad57
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2015-09-13 12:58:45 +0200

    Do not add context view to album view child list

--- src/view_albums.py
+++ src/view_albums.py
@@ -231,8 +231,6 @@ class AlbumsView(View):
         for child in self._albumbox.get_children():
             widget = child.get_child()
             children.append(widget)
-        if self._context_widget is not None:
-            children.append(self._context_widget)
         return children
 
     def _populate_context(self, album_id):
