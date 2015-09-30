375cc89ee370d95520e5715426601ecc1d463ad2
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2015-09-30 07:09:09 +0200

    Add missing AlbumContextWidget to children list

--- src/view_albums.py
+++ src/view_albums.py
@@ -118,6 +118,7 @@ class AlbumsView(View):
         for child in self._albumbox.get_children():
             widget = child.get_child()
             children.append(widget)
+        children.append(self._context_widget)
         return children
 
     def _populate_context(self, album_id):
