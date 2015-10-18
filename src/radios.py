9146417ca3be71f8dc805fca2df35c4433be8e13
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2015-10-18 19:00:31 +0200

    Do not quit when radios db is empty. Fix #326 #324

--- src/radios.py
+++ src/radios.py
@@ -182,7 +182,7 @@ class Radios(GObject.GObject):
                                         FROM radios\
                                         ORDER BY POPULARITY DESC LIMIT 100)")
             v = result.fetchone()
-            if v and v[0] > 5:
+            if v and v[0] is not None and v[0] > 5:
                 return v[0]
             return 5
 
