f21834bf2f74c02cd5b2e7175441b5bccc2e6417
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2016-03-30 19:21:28 +0200

    Disable selection in search popover

--- src/pop_search.py
+++ src/pop_search.py
@@ -195,6 +195,8 @@ class SearchPopover(Gtk.Popover):
         self._view = Gtk.ListBox()
         self._view.connect("button-press-event", self._on_button_press)
         self._view.connect("row-activated", self._on_row_activated)
+        self._view.set_selection_mode(Gtk.SelectionMode.NONE)
+        self._view.set_activate_on_single_click(True)
         self._view.show()
 
         builder.get_object('scrolled').add(self._view)
