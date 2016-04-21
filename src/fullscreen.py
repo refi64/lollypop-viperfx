a3d16ce5baf87a4751a77ff7a0da2c4aeeec085f
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2016-04-22 01:16:01 +0200

    Divide by scale when calculating cover size in fullscreen

--- src/fullscreen.py
+++ src/fullscreen.py
@@ -55,10 +55,11 @@ class FullScreen(Gtk.Window, InfosController,
         monitor = screen.get_primary_monitor()
         geometry = screen.get_monitor_geometry(monitor)
         # We want 500 and 200 in full hd
+        scale = self.get_scale_factor()
         if geometry.width > geometry.height:
-            artsize = int(ArtSize.MONSTER*geometry.width/1920)
+            artsize = int(ArtSize.MONSTER*geometry.width/1920/scale)
         else:
-            artsize = int(ArtSize.MONSTER*geometry.height/1920)
+            artsize = int(ArtSize.MONSTER*geometry.height/1920/scale)
         InfosController.__init__(self, artsize)
 
         self._play_btn = builder.get_object('play_btn')
