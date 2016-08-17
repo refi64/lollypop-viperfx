c7046010bb98281222137390efe7e77b6ed38e79
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2016-08-17 23:56:28 +0200

    Move from protected to private: toolbar.py

--- src/toolbar.py
+++ src/toolbar.py
@@ -33,25 +33,25 @@ class Toolbar(Gtk.HeaderBar):
             @param app as Gtk.Application
         """
         Gtk.HeaderBar.__init__(self)
-        self._width = WindowSize.SMALL
+        self.__width = WindowSize.SMALL
         self.set_title("Lollypop")
-        self._toolbar_playback = ToolbarPlayback()
-        self._toolbar_playback.show()
-        self._toolbar_info = ToolbarInfo()
-        self._toolbar_info.show()
-        self._toolbar_title = ToolbarTitle()
-        self._toolbar_title.show()
-        self._toolbar_end = ToolbarEnd(app)
-        self._toolbar_end.show()
-        self.pack_start(self._toolbar_playback)
-        self.pack_start(self._toolbar_info)
-        self.set_custom_title(self._toolbar_title)
-        self.pack_end(self._toolbar_end)
+        self.__toolbar_playback = ToolbarPlayback()
+        self.__toolbar_playback.show()
+        self.__toolbar_info = ToolbarInfo()
+        self.__toolbar_info.show()
+        self.__toolbar_title = ToolbarTitle()
+        self.__toolbar_title.show()
+        self.__toolbar_end = ToolbarEnd(app)
+        self.__toolbar_end.show()
+        self.pack_start(self.__toolbar_playback)
+        self.pack_start(self.__toolbar_info)
+        self.set_custom_title(self.__toolbar_title)
+        self.pack_end(self.__toolbar_end)
 
-        Lp().player.connect('status-changed', self._on_status_changed)
-        Lp().player.connect('current-changed', self._on_current_changed)
-        Lp().player.connect('next-changed', self._on_next_changed)
-        Lp().player.connect('prev-changed', self._on_prev_changed)
+        Lp().player.connect('status-changed', self.__on_status_changed)
+        Lp().player.connect('current-changed', self.__on_current_changed)
+        Lp().player.connect('next-changed', self.__on_next_changed)
+        Lp().player.connect('prev-changed', self.__on_prev_changed)
 
     def do_get_preferred_height(self):
         """
@@ -61,7 +61,7 @@ class Toolbar(Gtk.HeaderBar):
         """
         style = self.get_style_context()
         padding = style.get_padding(style.get_state())
-        info_height = self._toolbar_info.get_preferred_height()
+        info_height = self.__toolbar_info.get_preferred_height()
         if info_height[0]+padding.top+padding.bottom > 47:
             height = info_height[0]+padding.top+padding.bottom
         else:
@@ -73,23 +73,23 @@ class Toolbar(Gtk.HeaderBar):
             Allow snapping for screen with width > 1400
             @return (int, int)
         """
-        return (WindowSize.SMALL, self._width)
+        return (WindowSize.SMALL, self.__width)
 
     def set_content_width(self, window_width):
         """
             Calculate infos/title width
             @param window width as int
         """
-        width = self._toolbar_playback.get_preferred_width()[1]
+        width = self.__toolbar_playback.get_preferred_width()[1]
         if window_width < WindowSize.MONSTER:
-            self._toolbar_info.hide()
+            self.__toolbar_info.hide()
         else:
-            self._toolbar_info.show()
+            self.__toolbar_info.show()
         if window_width < WindowSize.BIG:
-            self._toolbar_end.hide()
+            self.__toolbar_end.hide()
         else:
-            width += self._toolbar_end.get_preferred_width()[1]
-            self._toolbar_end.show()
+            width += self.__toolbar_end.get_preferred_width()[1]
+            self.__toolbar_end.show()
         window = self.get_window()
         if window is not None:
             available = window.get_width() - width
@@ -98,24 +98,24 @@ class Toolbar(Gtk.HeaderBar):
                     title = available/2
                 else:
                     title = available
-                self._toolbar_title.set_width(title)
+                self.__toolbar_title.set_width(title)
                 if window_width >= WindowSize.MEDIUM:
-                    self._toolbar_info.set_width((available-title)/2)
-            self._width = window.get_width()
+                    self.__toolbar_info.set_width((available-title)/2)
+            self.__width = window.get_width()
 
     def update_position(self, value=None):
         """
             Update progress bar position
             @param value as int
         """
-        self._toolbar_title._update_position(value)
+        self.__toolbar_title._update_position(value)
 
     def setup_menu(self, menu):
         """
             Add an application menu to menu button
             @parma: menu as Gio.Menu
         """
-        self._toolbar_end.setup_menu(menu)
+        self.__toolbar_end.setup_menu(menu)
 
     def set_mark(self):
         """
@@ -124,7 +124,7 @@ class Toolbar(Gtk.HeaderBar):
         try:
             if Lp().settings.get_value('save-state'):
                 position = load(open(DataPath + "/position.bin", "rb"))
-                self._toolbar_title.add_mark(position/1000000)
+                self.__toolbar_title.add_mark(position/1000000)
         except Exception as e:
             print("Toolbar::restore_state():", e)
 
@@ -133,41 +133,41 @@ class Toolbar(Gtk.HeaderBar):
         """
             Art size as int
         """
-        return self._toolbar_info.artsize
+        return self.__toolbar_info.artsize
 
 #######################
 # PRIVATE             #
 #######################
-    def _on_current_changed(self, player):
+    def __on_current_changed(self, player):
         """
             Update toolbar
             @param player as Player
         """
         debug("Toolbar::_on_current_changed")
-        self._toolbar_playback.on_current_changed(player)
-        self._toolbar_info.on_current_changed(player)
-        self._toolbar_title.on_current_changed(player)
+        self.__toolbar_playback.on_current_changed(player)
+        self.__toolbar_info.on_current_changed(player)
+        self.__toolbar_title.on_current_changed(player)
 
-    def _on_prev_changed(self, player):
+    def __on_prev_changed(self, player):
         """
             Update toolbar
             @param player as Player
         """
-        self._toolbar_playback.on_prev_changed(player)
+        self.__toolbar_playback.on_prev_changed(player)
 
-    def _on_next_changed(self, player):
+    def __on_next_changed(self, player):
         """
             Update toolbar
             @param player as Player
         """
-        self._toolbar_playback.on_next_changed(player)
-        self._toolbar_end.on_next_changed(player)
+        self.__toolbar_playback.on_next_changed(player)
+        self.__toolbar_end.on_next_changed(player)
 
-    def _on_status_changed(self, player):
+    def __on_status_changed(self, player):
         """
             Update buttons and progress bar
             @param player as Player
         """
-        self._toolbar_playback.on_status_changed(player)
-        self._toolbar_title.on_status_changed(player)
-        self._toolbar_end.on_status_changed(player)
+        self.__toolbar_playback.on_status_changed(player)
+        self.__toolbar_title.on_status_changed(player)
+        self.__toolbar_end.on_status_changed(player)
