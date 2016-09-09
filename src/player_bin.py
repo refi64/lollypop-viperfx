0fb0e036d612bfafe955c7c572bc1497039396a6
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2016-09-09 19:47:10 +0200

    Check if duration is valid

--- src/player_bin.py
+++ src/player_bin.py
@@ -340,7 +340,7 @@ class BinPlayer(BasePlayer):
         """
         try:
             duration = reader.get_info(track.uri).get_duration() / 1000000000
-            if duration != track.duration:
+            if duration != track.duration and duration > 0:
                 Lp().tracks.set_duration(track.id, duration)
                 # We modify mtime to be sure not looking for tags again
                 Lp().tracks.set_mtime(track.id, 1)
