28c64692aa3c1312a35c6f0ab92f493b6e2ae46e
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2016-09-18 19:54:25 +0200

    Fix a stupid error in Track object. And remove an unused function. #736

--- src/objects.py
+++ src/objects.py
@@ -247,8 +247,8 @@ class Track(Base):
     """
     FIELDS = ['name', 'album_id', 'album_artist_ids',
               'artist_ids', 'genre_ids', 'album_name', 'artists', 'genres',
-              'duration', 'number', 'position', 'year', 'persistent', 'mtime']
-    DEFAULTS = ['', None, [], [], [], '', '', 0.0, None, 0, None, 1, 0]
+              'duration', 'number', 'year', 'persistent', 'mtime']
+    DEFAULTS = ['', None, [], [], [], '', '', '', 0.0, 0, None, 1, 0]
 
     def __init__(self, track_id=None):
         """
