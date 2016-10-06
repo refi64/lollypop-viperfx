45a169454926412d33dba4ba085edffc5b5775de
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2016-10-06 13:46:59 +0200

    Revert "Remove set_homogeneous(), works without"
    This reverts commit db1e410087037a5f5f88344845263f7d5f26b986.

--- src/view_radios.py
+++ src/view_radios.py
@@ -49,6 +49,7 @@ class RadiosView(LazyLoadingView):
 
         self.__radiobox = Gtk.FlowBox()
         self.__radiobox.set_selection_mode(Gtk.SelectionMode.NONE)
+        self.__radiobox.set_homogeneous(True)
         self.__radiobox.set_max_children_per_line(1000)
         self.__radiobox.show()
 
