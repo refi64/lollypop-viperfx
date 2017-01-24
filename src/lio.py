d39088a5db03b518e184018efa1ffaa2fbaf879e
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2017-01-24 13:39:38 +0100

    Add Soup require version

--- src/lio.py
+++ src/lio.py
@@ -10,6 +10,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+import gi
+gi.require_version('Soup', '2.4')
+
 from gi.repository import Gio, GObject, Soup
 
 from lollypop.define import Lp
