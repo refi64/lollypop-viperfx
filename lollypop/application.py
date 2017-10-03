5c00b66b96cfd7be2f97c32ea28cbe29f3e93a6b
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2017-10-03 17:02:38 +0200

    Restore Ubuntu Xenial support. Fix #1255

--- lollypop/application.py
+++ lollypop/application.py
@@ -149,8 +149,12 @@ class Application(Gtk.Application):
         except Exception as e:
             print("Application::init():", e)
 
-        cssProviderFile = Gio.File.new_for_uri(
-                "resource:///org/gnome/Lollypop/application.css")
+        if Gtk.get_minor_version() > 18:
+            cssProviderFile = Gio.File.new_for_uri(
+                 "resource:///org/gnome/Lollypop/application.css")
+        else:
+            cssProviderFile = Gio.File.new_for_uri(
+                 "resource:///org/gnome/Lollypop/application-legacy.css")
         cssProvider = Gtk.CssProvider()
         cssProvider.load_from_file(cssProviderFile)
         screen = Gdk.Screen.get_default()
