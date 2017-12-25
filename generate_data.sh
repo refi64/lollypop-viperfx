af390e9c8c76c6f2ed7d8972dbcd188770afe180
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2017-12-25 11:12:33 +0100

    Update generate_data.sh

--- generate_data.sh
+++ generate_data.sh
@@ -26,14 +26,31 @@ function generate_pot()
     echo '[encoding: UTF-8]'
     for file in data/*.xml data/*.in lollypop/*.py
     do
-        echo ../$file
+        echo $file
     done
     for file in data/*.ui data/AboutDialog.ui.in
     do
         echo -n '[type: gettext/glade]'
-        echo ../$file
+        echo $file
     done
+}
+
+function generate_po()
+{
+    cd po
+    git pull https://hosted.weblate.org/git/gnumdk/lollypop
+    intltool-update --pot
+    mv -f untitled.pot lollypop.pot
+    >LINGUAS
+    for file in *.po
+    do
+        po=${file%.po}
+        echo $po >> LINGUAS
+        intltool-update $po
+    done
+    cd -
 }
 
 generate_resource > data/lollypop.gresource.xml
 generate_pot > po/POTFILES.in
+generate_po
