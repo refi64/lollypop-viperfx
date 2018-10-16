72c4e278a7cd19a5666f6e88c0a969b7b61fbad8
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2018-10-16 14:25:15 +0200

    Update popover sizing

--- lollypop/pop_artwork.py
+++ lollypop/pop_artwork.py
@@ -38,8 +38,8 @@ class CommonPopover(Gtk.Popover):
             @param widget as Gtk.Widget
         """
         size = App().window.get_size()
-        self.set_size_request(size[0] * 0.4,
-                              size[1] * 0.5)
+        self.set_size_request(size[0] * 0.8,
+                              size[1] * 0.6)
 
     def __on_unmap(self, widget):
         """
