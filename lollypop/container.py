8c2dc2387125c3d3e5f383b70b06bb5d46b84548
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2018-11-18 15:04:47 +0100

    Exclude DeviceView

--- lollypop/container.py
+++ lollypop/container.py
@@ -125,9 +125,11 @@ class Container(Gtk.Overlay):
         """
             Reload current view
         """
-        # Destroy all widgets
+        from lollypop.view_device import DeviceView
+        # Destroy all widgets except DeviceView
         for child in self.__stack.get_children():
-            child.destroy()
+            if not isinstance(child, DeviceView):
+                child.destroy()
         if App().settings.get_value("show-sidebar"):
             self.__reload_list_view()
         else:
