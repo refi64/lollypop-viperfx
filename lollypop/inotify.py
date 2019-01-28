4a970ffbb72870cdf6438023aebe1686d1f5f789
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2019-01-28 14:20:54 +0100

    Check file exists

--- lollypop/inotify.py
+++ lollypop/inotify.py
@@ -67,9 +67,10 @@ class Inotify:
                 self.__monitors[changed_uri] == monitor:
             return
         # Ignore non audio/dir
-        if not is_audio(changed_file) and\
-            changed_file.query_file_type(Gio.FileQueryInfoFlags.NONE,
-                                         None) != Gio.FileType.DIRECTORY:
+        if changed_file.query_exists() and\
+                not is_audio(changed_file) and\
+                changed_file.query_file_type(Gio.FileQueryInfoFlags.NONE,
+                                             None) != Gio.FileType.DIRECTORY:
             return
 
         # Stop collection scanner and wait
