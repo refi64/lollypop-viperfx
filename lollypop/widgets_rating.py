5e8a54f25545ae778fa3090ab295b6ec9648ceb0
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2019-02-01 16:02:31 +0100

    More fixes for librem

--- lollypop/widgets_rating.py
+++ lollypop/widgets_rating.py
@@ -33,6 +33,7 @@ class RatingWidget(Gtk.Bin):
         Gtk.Bin.__init__(self)
         self.__object = object
         self.set_property("valign", Gtk.Align.CENTER)
+        self.set_vexpand(True)
         builder = Gtk.Builder()
         builder.add_from_resource("/org/gnome/Lollypop/RatingWidget.ui")
         builder.connect_signals(self)
