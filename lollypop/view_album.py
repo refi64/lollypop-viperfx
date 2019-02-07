c705c494db4a376db60e031be4d78c1405bf5c9f
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2019-02-07 13:31:38 +0100

    Handle albums with multiple artists

--- lollypop/view_album.py
+++ lollypop/view_album.py
@@ -37,34 +37,15 @@ class AlbumView(View, TracksView):
         view_type = ViewType.TWO_COLUMNS | ViewType.MULTIPLE
         View.__init__(self, view_type)
         TracksView.__init__(self, view_type)
+        self._album = album
+        self.__genre_ids = genre_ids
+        self.__artist_ids = artist_ids
         self.__grid = Gtk.Grid()
         self.__grid.set_property("vexpand", True)
         self.__grid.set_row_spacing(10)
         self.__grid.set_orientation(Gtk.Orientation.VERTICAL)
         self.__grid.show()
-        self._album = album
-        self.__artist_ids = artist_ids
         self.__genre_ids = remove_static_genres(genre_ids)
-        if artist_ids and artist_ids[0] == Type.COMPILATIONS:
-            self.__album_ids = App().albums.get_compilation_ids(genre_ids)
-        else:
-            self.__album_ids = App().albums.get_ids(artist_ids, genre_ids)
-        self.__label = None
-        if len(self.__album_ids) == 1 or len(artist_ids) != 1:
-            return
-        # Remove album from artist albums
-        if self._album.id in self.__album_ids:
-            self.__album_ids.remove(album.id)
-        artist = App().artists.get_name(artist_ids[0])
-        self.__label = Gtk.Label()
-        self.__label.set_markup(
-                         '''<span size="large" alpha="40000"
-                             weight="bold">%s %s</span>''' %
-                         (_("Others albums from"), artist))
-        self.__label.set_property("halign", Gtk.Align.START)
-        self.__label.set_margin_top(30)
-        self.__label.set_margin_start(20)
-        self.__label.show()
 
     def populate(self):
         """
@@ -104,16 +85,34 @@ class AlbumView(View, TracksView):
         """
         if TracksView.get_populated(self):
             from lollypop.view_albums_box import AlbumsBoxView
-            if self.__label is not None:
-                self.__grid.add(self.__label)
-                others_box = AlbumsBoxView(self.__genre_ids,
-                                           self.__artist_ids,
+            for artist_id in self.__artist_ids:
+                artist = App().artists.get_name(artist_id)
+                label = Gtk.Label()
+                label.set_markup(
+                                 '''<span size="large" alpha="40000"
+                                     weight="bold">%s %s</span>''' %
+                                 (_("Others albums from"), artist))
+                label.set_property("halign", Gtk.Align.START)
+                label.set_margin_top(30)
+                label.set_margin_start(20)
+                label.show()
+                self.__grid.add(label)
+                others_box = AlbumsBoxView([],
+                                           [artist_id],
                                            ViewType.SMALL)
                 others_box.show()
                 others_box.set_margin_start(30)
                 others_box.set_margin_end(30)
                 self.__grid.add(others_box)
-                others_box.populate([Album(id) for id in self.__album_ids])
+                if artist_id == Type.COMPILATIONS:
+                    album_ids = App().albums.get_compilation_ids(
+                        self.__genre_ids)
+                else:
+                    album_ids = App().albums.get_ids(
+                        [artist_id], [])
+                if self._album.id in album_ids:
+                    album_ids.remove(self._album.id)
+                others_box.populate([Album(id) for id in album_ids])
         else:
             TracksView.populate(self)
 
