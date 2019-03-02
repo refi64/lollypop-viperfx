3c3fc854537de65b8e19c6862ff051c766750401
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2019-03-02 16:17:12 +0100

    Add youtube-scale CSS class

--- lollypop/controller_progress.py
+++ lollypop/controller_progress.py
@@ -35,7 +35,8 @@ class ProgressController:
             Update scale on current changed
             @param player as Player
         """
-        self._progress.clear_marks()
+        style_context = self._progress.get_style_context()
+        style_context.remove_class("youtube-scale")
         if player.current_track.id is None:
             self._progress.set_sensitive(False)
             self._total_time_label.set_text("")
@@ -49,6 +50,8 @@ class ProgressController:
             self._total_time_label.set_text("")
             self._progress.set_range(0.0, 0.0)
         else:
+            if player.current_track.mtime <= 0:
+                style_context.add_class("youtube-scale")
             self._progress.set_sensitive(True)
             self._progress.set_range(0.0, player.current_track.duration)
             self._total_time_label.set_text(
