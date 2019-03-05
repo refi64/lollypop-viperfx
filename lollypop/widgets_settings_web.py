2fe650f6d7fb8689f19a6a584f7373e42bb601e1
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2019-03-05 18:00:46 +0100

    Last.fm module cleanup 2

--- lollypop/widgets_settings_web.py
+++ lollypop/widgets_settings_web.py
@@ -104,8 +104,6 @@ class WebSettingsWidget(Gtk.Bin):
             Test lastfm connection
             @param button as Gtk.Button
         """
-        App().settings.set_value("lastfm-loved-status",
-                                 GLib.Variant("b", False))
         self.__update_fm_settings("lastfm")
         if not Gio.NetworkMonitor.get_default().get_network_available():
             self.__lastfm_test_image.set_from_icon_name(
